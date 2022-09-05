6511e7abc77d0fc823cdb215e56f18d1ce4c777c
Nikolai <60686353+cod3-tam3r@users.noreply.github.com>
2022-09-05 14:56:28 +0300

    distance update

--- levenshtein-distance.js
+++ levenshtein-distance.js
@@ -1,28 +1,31 @@
 let word1 = "лабрадор",
   word2 = "гибралтар";
 
-let matrix = [];
-let a, b, c;
+function distance(word1, word2) {
+  let matrix = [];
+  let a, b, c;
 
-for (let i = 0; i < word2.length + 1; i++) {
-  matrix.push([]);
-  for (let j = 0; j < word1.length + 1; j++) {
-    if (j == 0 && i > 0) {
-      matrix[i].push(i);
-    } else {
-      if (i == 0) {
-        matrix[i].push(j);
+  for (let i = 0; i < word2.length + 1; i++) {
+    matrix.push([]);
+    for (let j = 0; j < word1.length + 1; j++) {
+      if (j == 0 && i > 0) {
+        matrix[i].push(i);
       } else {
-        a = matrix[i - 1][j] + 1;
-        b = matrix[i][j - 1] + 1;
-        c = matrix[i - 1][j - 1] + (word1[j - 1] !== word2[i - 1] ? 1 : 0);
-        matrix[i][j] = Math.min(a, Math.min(b, c));
+        if (i == 0) {
+          matrix[i].push(j);
+        } else {
+          a = matrix[i - 1][j] + 1;
+          b = matrix[i][j - 1] + 1;
+          c = matrix[i - 1][j - 1] + (word1[j - 1] !== word2[i - 1] ? 1 : 0);
+          matrix[i][j] = Math.min(a, Math.min(b, c));
+        }
       }
     }
   }
+  return matrix[word2.length][word1.length];
 }
 
 console.log(
   `Levenshtein distance ${word1}, ${word2} is`,
-  matrix[word2.length - 1][word1.length - 1]
+  distance(word1, word2)
 );
